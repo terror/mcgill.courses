20dd730b0da9340931521d3ec12404bd8b5c8e4e
Liam <liam@scalzulli.com>
2023-03-23 19:35:29 -0400

    Refactor auth routes

--- src/auth.rs
+++ src/auth.rs
@@ -17,45 +17,18 @@ pub struct AuthRequest {
   state: String,
 }
 
-pub(crate) fn oauth_client() -> BasicClient {
-  let client_id = env::var("MS_CLIENT_ID")
-    .expect("Missing the MS_CLIENT_ID environment variable");
-  let client_secret = env::var("MS_CLIENT_SECRET")
-    .expect("Missing the MS_CLIENT_SECRET environment variable.");
-  let auth_url = AuthUrl::new(
-    "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
-      .to_string(),
-  )
-  .expect("Invalid authorization URL");
-  let token_url = TokenUrl::new(
-    "https://login.microsoftonline.com/common/oauth2/v2.0/token".to_string(),
-  )
-  .expect("Invalid token endpoint URL");
-  let redirect_uri = env::var("MS_REDIRECT_URI")
-    .expect("Missing the MS_REDIRECT_URI environment variable.");
-
-  BasicClient::new(
-    ClientId::new(client_id),
-    Some(ClientSecret::new(client_secret)),
-    auth_url,
-    Some(token_url),
-  )
-  .set_auth_type(AuthType::RequestBody)
-  .set_redirect_uri(
-    RedirectUrl::new(redirect_uri).expect("Invalid redirect URL"),
-  )
-}
-
 pub(crate) async fn microsoft_auth(
   AppState(client): AppState<BasicClient>,
 ) -> impl IntoResponse {
-  let (authorize_url, _csrf_token) = client
-    .authorize_url(CsrfToken::new_random)
-    .add_scope(Scope::new(String::from("openid")))
-    .add_scope(Scope::new(String::from("User.Read")))
-    .url();
-
-  Redirect::to(authorize_url.as_ref())
+  Redirect::to(
+    client
+      .authorize_url(CsrfToken::new_random)
+      .add_scope(Scope::new(String::from("openid")))
+      .add_scope(Scope::new(String::from("User.Read")))
+      .url()
+      .0
+      .as_ref(),
+  )
 }
 
 pub(crate) async fn login_authorized(
@@ -63,46 +36,65 @@ pub(crate) async fn login_authorized(
   AppState(store): AppState<MemoryStore>,
   AppState(oauth_client): AppState<BasicClient>,
 ) -> Result<impl IntoResponse> {
+  log::debug!("Fetching token from oauth client...");
+
   let token = oauth_client
     .exchange_code(AuthorizationCode::new(query.code.clone()))
     .request_async(async_http_client)
     .await?;
 
   let client = reqwest::Client::new();
-  let user = client
-    .get("https://graph.microsoft.com/v1.0/me")
-    .bearer_auth(token.access_token().secret())
-    .send()
-    .await?
-    .json::<User>()
-    .await?;
 
   let mut session = Session::new();
-  session.insert("user", &user)?;
 
-  let cookie = store.store_session(session).await?.unwrap();
-  let cookie = format!("{}={}; SameSite=Lax; Path=/", COOKIE_NAME, cookie);
+  log::debug!("Inserting user data into session...");
+
+  session.insert(
+    "user",
+    &client
+      .get("https://graph.microsoft.com/v1.0/me")
+      .bearer_auth(token.access_token().secret())
+      .send()
+      .await?
+      .json::<User>()
+      .await?,
+  )?;
 
   let mut headers = HeaderMap::new();
-  headers.insert(SET_COOKIE, cookie.parse()?);
 
-  Ok((headers, Redirect::to("http://localhost:5173")))
+  headers.insert(
+    SET_COOKIE,
+    format!(
+      "{}={}; SameSite=Lax; Path=/",
+      COOKIE_NAME,
+      store
+        .store_session(session)
+        .await?
+        .ok_or(anyhow!("Failed to store session"))?
+    )
+    .parse()?,
+  );
+
+  Ok((headers, Redirect::to(CLIENT_URL)))
 }
 
 pub(crate) async fn logout(
   TypedHeader(cookies): TypedHeader<Cookie>,
-  AppState(store): AppState<MemoryStore>,
+  AppState(session_store): AppState<MemoryStore>,
 ) -> Result<impl IntoResponse> {
   let cookie = match cookies.get(COOKIE_NAME) {
     Some(c) => c,
-    None => return Ok(Redirect::to("http://localhost:5173")),
+    None => return Ok(Redirect::to(CLIENT_URL)),
   };
 
-  let session = match store.load_session(cookie.to_string()).await? {
+  let session = match session_store.load_session(cookie.to_string()).await? {
     Some(s) => s,
-    None => return Ok(Redirect::to("http://localhost:5173")),
+    None => return Ok(Redirect::to(CLIENT_URL)),
   };
 
-  store.destroy_session(session).await?;
-  Ok(Redirect::to("http://localhost:5173"))
+  log::debug!("Destroying session...");
+
+  session_store.destroy_session(session).await?;
+
+  Ok(Redirect::to(CLIENT_URL))
 }
