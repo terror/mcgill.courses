8b8b2517aa875d0b82505fd0c47d05bd16edadc2
Liam <liam@scalzulli.com>
2023-03-25 16:31:05 -0400

    Format

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -10,20 +10,27 @@ export const Explore = () => {
   useEffect(() => {
     fetchClient
       .getData<Course[]>('/courses')
-      .then((courses) => setCourses(courses.filter(course => course.title !== '')))
+      .then((courses) =>
+        setCourses(courses.filter((course) => course.title !== ''))
+      )
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <Layout>
-        <div className='max-w-xl py-32 sm:py-20 lg:py-56'>
-          <h1 className='m-5 text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
-            Showing all courses
-          </h1>
-          {courses.map((course) => (
-            <div className='p-2 border rounded-lg m-2' key={course._id}><div className='font-bold'>{course._id} - {course.title}</div><div>{course.description}</div></div>
-          ))}
-        </div>
+      <div className='max-w-xl py-32 sm:py-20 lg:py-56'>
+        <h1 className='m-5 text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+          Showing all courses
+        </h1>
+        {courses.map((course) => (
+          <div className='p-2 border rounded-lg m-2' key={course._id}>
+            <div className='font-bold'>
+              {course._id} - {course.title}
+            </div>
+            <div>{course.description}</div>
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
