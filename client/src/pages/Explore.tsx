bf496338fc58a82dd90e6cb80fead9ffc313404d
Liam <liam@scalzulli.com>
2023-03-25 16:40:02 -0400

    Center stuff

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -18,18 +18,23 @@ export const Explore = () => {
 
   return (
     <Layout>
-      <div className='max-w-xl py-32 sm:py-20 lg:py-56'>
+      <div className='w-full py-32 flex flex-col items-center'>
         <h1 className='m-5 text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
           Showing all courses
         </h1>
-        {courses.map((course) => (
-          <div className='p-2 border rounded-lg m-2' key={course._id}>
-            <div className='font-bold'>
-              {course._id} - {course.title}
+        <div className='mx-auto'>
+          {courses.map((course) => (
+            <div
+              className='max-w-xl p-5 border rounded-lg m-2'
+              key={course._id}
+            >
+              <div className='font-bold mb-2'>
+                {course._id} - {course.title}
+              </div>
+              <div>{course.description}</div>
             </div>
-            <div>{course.description}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </Layout>
   );
