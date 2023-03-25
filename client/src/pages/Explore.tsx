8d1a968fa24f4dc535a55e9c70be0b6336ec8e20
Liam <liam@scalzulli.com>
2023-03-25 17:11:06 -0400

    Loading indicator

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -6,14 +6,19 @@ import { fetchClient } from '../lib/fetchClient';
 
 export const Explore = () => {
   const [courses, setCourses] = useState<Course[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchClient
       .getData<Course[]>('/courses')
-      .then((courses) =>
-        setCourses(courses.filter((course) => course.title !== ''))
-      )
-      .catch((err) => console.log(err));
+      .then((courses) => {
+        setCourses(courses.filter((course) => course.title !== ''));
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -22,28 +27,32 @@ export const Explore = () => {
         <h1 className='m-5 text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
           Showing all courses
         </h1>
-        <div className='mx-auto'>
-          {courses.map((course) => (
-            <div
-              className='max-w-xl p-5 border rounded-lg m-2'
-              key={course._id}
-            >
-              <div className='font-bold mb-2'>
-                {course._id} - {course.title}
-              </div>
-              <div>{course.description}</div>
-              {course.instructors.length !== 0 && (
-                <div className='mt-2'>
-                  {course.instructors.map((instructor) => (
-                    <span>
-                      {instructor.name} ({instructor.term}){' '}
-                    </span>
-                  ))}
+        {loading ? (
+          <p>🌀 Loading...</p>
+        ) : (
+          <div className='mx-auto'>
+            {courses.map((course) => (
+              <div
+                className='max-w-xl p-5 border rounded-lg m-2'
+                key={course._id}
+              >
+                <div className='font-bold mb-2'>
+                  {course._id} - {course.title}
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
+                <div>{course.description}</div>
+                {course.instructors.length !== 0 && (
+                  <div className='mt-2'>
+                    {course.instructors.map((instructor) => (
+                      <span>
+                        {instructor.name} ({instructor.term}){' '}
+                      </span>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   );
