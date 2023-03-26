3ef1898480086e10de2547490b595d310c206db8
Liam <liam@scalzulli.com>
2023-03-25 22:47:37 -0400

    Add courses pagination

--- crates/db/src/db.rs
+++ crates/db/src/db.rs
@@ -89,12 +89,19 @@ impl Db {
     Ok(())
   }
 
-  pub async fn courses(&self) -> Result<Vec<Course>> {
+  pub async fn courses(
+    &self,
+    limit: Option<i64>,
+    offset: Option<u64>,
+  ) -> Result<Vec<Course>> {
     Ok(
       self
         .database
         .collection::<Course>(Db::COURSE_COLLECTION)
-        .find(None, None)
+        .find(
+          None,
+          FindOptions::builder().skip(offset).limit(limit).build(),
+        )
         .await?
         .try_collect::<Vec<Course>>()
         .await?,
@@ -226,17 +233,17 @@ mod tests {
   async fn on_disk_database_is_persistent() {
     let TestContext { db, db_name } = TestContext::new().await;
 
-    assert_eq!(db.courses().await.unwrap().len(), 0);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 0);
 
     db.add_course(Course::default()).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 1);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 1);
 
     drop(db);
 
     let db = Db::connect(&db_name).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 1);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 1);
   }
 
   #[tokio::test(flavor = "multi_thread")]
@@ -251,7 +258,7 @@ mod tests {
 
     db.seed(source).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 2);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 2);
   }
 
   #[tokio::test(flavor = "multi_thread")]
@@ -273,7 +280,7 @@ mod tests {
 
     db.seed(source).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 1);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 1);
   }
 
   #[tokio::test(flavor = "multi_thread")]
@@ -288,13 +295,13 @@ mod tests {
 
     db.seed(source.clone()).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 2);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 2);
 
     fs::write(&source, get_content("update.json")).unwrap();
 
     db.seed(source).await.unwrap();
 
-    let courses = dbg!(db.courses().await.unwrap());
+    let courses = dbg!(db.courses(None, None).await.unwrap());
 
     assert_eq!(courses.len(), 3);
 
@@ -317,7 +324,7 @@ mod tests {
 
     db.seed(source.clone()).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 83);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 83);
 
     let courses = db.search("COMP 202").await.unwrap();
 
@@ -341,7 +348,7 @@ mod tests {
 
     db.seed(source.clone()).await.unwrap();
 
-    let courses = db.courses().await.unwrap();
+    let courses = db.courses(None, None).await.unwrap();
 
     assert_eq!(courses.len(), 83);
 
@@ -365,7 +372,7 @@ mod tests {
 
     db.seed(source.clone()).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 83);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 83);
 
     let courses = db.search("COMP202").await.unwrap();
 
@@ -389,7 +396,7 @@ mod tests {
 
     db.seed(source.clone()).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 83);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 83);
 
     let courses = db.search("foundations of").await.unwrap();
 
@@ -413,7 +420,7 @@ mod tests {
 
     db.seed(source.clone()).await.unwrap();
 
-    assert_eq!(db.courses().await.unwrap().len(), 83);
+    assert_eq!(db.courses(None, None).await.unwrap().len(), 83);
 
     let courses = db.search("computing systems").await.unwrap();
 
@@ -423,5 +430,35 @@ mod tests {
 
     assert_eq!(first.subject, "COMP");
     assert_eq!(first.code, "350");
+  }
+
+  #[tokio::test(flavor = "multi_thread")]
+  async fn get_courses_with_limit() {
+    let TestContext { db, db_name } = TestContext::new().await;
+
+    let tempdir = TempDir::new(&db_name).unwrap();
+
+    let source = tempdir.path().join("courses.json");
+
+    fs::write(&source, get_content("search.json")).unwrap();
+
+    db.seed(source.clone()).await.unwrap();
+
+    assert_eq!(db.courses(Some(10), None).await.unwrap().len(), 10);
+  }
+
+  #[tokio::test(flavor = "multi_thread")]
+  async fn get_courses_with_offset() {
+    let TestContext { db, db_name } = TestContext::new().await;
+
+    let tempdir = TempDir::new(&db_name).unwrap();
+
+    let source = tempdir.path().join("courses.json");
+
+    fs::write(&source, get_content("search.json")).unwrap();
+
+    db.seed(source.clone()).await.unwrap();
+
+    assert_eq!(db.courses(None, Some(20)).await.unwrap().len(), 63);
   }
 }
