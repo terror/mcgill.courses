a5df65d32f4aa1985611d5e3805d2356a867108c
JeffDotPng <zhangjeff2014@gmail.com>
2023-03-25 21:22:29 -0400

    add course page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import { CoursePage } from './pages/CoursePage';
 import { Home } from './pages/Home';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='course'>
+        <Route path=':id' element={<CoursePage />} />
+      </Route>
     </Routes>
   );
 };
