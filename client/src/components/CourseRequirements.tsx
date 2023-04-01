74ca05ad3938fac2d252b37c413f17a9ab97ceaa
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-01 16:42:24 -0400

    format codes for course requirements

--- client/src/components/CourseRequirements.tsx
+++ client/src/components/CourseRequirements.tsx
@@ -3,51 +3,34 @@ import { Requirements } from '../model/requirements';
 type RequirementsProps = {
   requirements: Requirements;
 };
+
+type InfoBlockProps = {
+  title: string;
+  elements: string[];
+};
+
+export const InfoBlock = ({ title, elements }: InfoBlockProps) => {
+  return (
+    <div>
+      <h2 className='leading-none mt-1 mb-2 font-semibold text-gray-700'>
+        {title}
+      </h2>
+      {elements.map((element) => (
+        <p className='text-gray-500'>{element}</p>
+      ))}
+    </div>
+  );
+};
+
 export const CourseRequirements = ({ requirements }: RequirementsProps) => {
   return (
     <div className='w-screen  mx-4 p-6 bg-slate-50 rounded-md md:w-1/3 md:mt-10 md:ml-auto md:mr-10'>
       <div className='flex-col space-y-3'>
         <div className='space-y-7'>
-          {requirements.prereqs.length > 0 ? (
-            <div>
-              <h2 className='leading-none mt-1 font-semibold text-gray-700'>
-                Prerequisites
-              </h2>
-              {requirements.prereqs.map((prereq) => (
-                <p className='text-gray-500'>{prereq}</p>
-              ))}
-            </div>
-          ): null}
-          {requirements.coreqs.length > 0 ? (
-            <div>
-              <h2 className='leading-none mt-1 font-semibold text-gray-700'>
-                Corequisites
-              </h2>
-              {requirements.coreqs.map((coreq) => (
-                <p className='text-gray-500'>{coreq}</p>
-              ))}
-            </div>
-          ): null}
-          {requirements.restrictions.length > 0 ? (
-            <div>
-              <h2 className='leading-none mt-1 font-semibold text-gray-700'>
-                Restrictions
-              </h2>
-              {requirements.restrictions.map((restriction) => (
-                <p className='text-gray-500'>{restriction}</p>
-              ))}
-            </div>
-          ): null}
-          {requirements.otherInformation.length > 0 ? (
-            <div>
-              <h2 className='leading-none mt-1 font-semibold text-gray-700'>
-                Other Information
-              </h2>
-              {requirements.otherInformation.map((info) => (
-                <p className='text-gray-500'>{info}</p>
-              ))}
-            </div>
-          ): null}
+          {requirements.prereqs.length > 0 ? <InfoBlock title='Prerequisites' elements={requirements.prereqs} /> : null}
+          {requirements.coreqs.length > 0 ?  <InfoBlock title='Corequisites' elements={requirements.coreqs} /> : null}
+          {requirements.restrictions.length > 0 ? <InfoBlock title='Restrictions' elements={requirements.restrictions} /> : null}
+          {requirements.otherInformation.length > 0 ? <InfoBlock title='Other Information' elements={requirements.otherInformation} /> : null}
         </div>
       </div>
     </div>
