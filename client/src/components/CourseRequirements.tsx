5b640718489e25945674e60f6c4772f75b98898c
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-01 14:21:48 -0400

    improve responsiveness

--- client/src/components/CourseRequirements.tsx
+++ client/src/components/CourseRequirements.tsx
@@ -5,7 +5,7 @@ type RequirementsProps = {
 };
 export const CourseRequirements = ({ requirements }: RequirementsProps) => {
   return (
-    <div className='w-screen md:w-1/3 md:mt-10 mx-4 p-6 bg-slate-50 rounded-md flex ml-auto mr-10'>
+    <div className='w-screen  mx-4 p-6 bg-slate-50 rounded-md md:w-1/3 md:mt-10 md:ml-auto md:mr-10'>
       <div className='flex-col space-y-3'>
         <div className='space-y-7'>
           {requirements.prereqs.length > 0 && (
