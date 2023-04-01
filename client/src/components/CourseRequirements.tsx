b680d5c5252e171185692c1755150c439b00928e
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-01 16:30:01 -0400

    minor format

--- client/src/components/CourseRequirements.tsx
+++ client/src/components/CourseRequirements.tsx
@@ -8,7 +8,7 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
     <div className='w-screen  mx-4 p-6 bg-slate-50 rounded-md md:w-1/3 md:mt-10 md:ml-auto md:mr-10'>
       <div className='flex-col space-y-3'>
         <div className='space-y-7'>
-          {requirements.prereqs.length > 0 && (
+          {requirements.prereqs.length > 0 ? (
             <div>
               <h2 className='leading-none mt-1 font-semibold text-gray-700'>
                 Prerequisites
@@ -17,8 +17,8 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
                 <p className='text-gray-500'>{prereq}</p>
               ))}
             </div>
-          )}
-          {requirements.coreqs.length > 0 && (
+          ): null}
+          {requirements.coreqs.length > 0 ? (
             <div>
               <h2 className='leading-none mt-1 font-semibold text-gray-700'>
                 Corequisites
@@ -27,8 +27,8 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
                 <p className='text-gray-500'>{coreq}</p>
               ))}
             </div>
-          )}
-          {requirements.restrictions.length > 0 && (
+          ): null}
+          {requirements.restrictions.length > 0 ? (
             <div>
               <h2 className='leading-none mt-1 font-semibold text-gray-700'>
                 Restrictions
@@ -37,8 +37,8 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
                 <p className='text-gray-500'>{restriction}</p>
               ))}
             </div>
-          )}
-          {requirements.otherInformation.length > 0 && (
+          ): null}
+          {requirements.otherInformation.length > 0 ? (
             <div>
               <h2 className='leading-none mt-1 font-semibold text-gray-700'>
                 Other Information
@@ -47,7 +47,7 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
                 <p className='text-gray-500'>{info}</p>
               ))}
             </div>
-          )}
+          ): null}
         </div>
       </div>
     </div>
