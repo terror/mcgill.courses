f2689a4df95c2b35bbe29a47ed03a32b62918538
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-01 16:42:42 -0400

    format

--- client/src/components/CourseRequirements.tsx
+++ client/src/components/CourseRequirements.tsx
@@ -27,10 +27,24 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
     <div className='w-screen  mx-4 p-6 bg-slate-50 rounded-md md:w-1/3 md:mt-10 md:ml-auto md:mr-10'>
       <div className='flex-col space-y-3'>
         <div className='space-y-7'>
-          {requirements.prereqs.length > 0 ? <InfoBlock title='Prerequisites' elements={requirements.prereqs} /> : null}
-          {requirements.coreqs.length > 0 ?  <InfoBlock title='Corequisites' elements={requirements.coreqs} /> : null}
-          {requirements.restrictions.length > 0 ? <InfoBlock title='Restrictions' elements={requirements.restrictions} /> : null}
-          {requirements.otherInformation.length > 0 ? <InfoBlock title='Other Information' elements={requirements.otherInformation} /> : null}
+          {requirements.prereqs.length > 0 ? (
+            <InfoBlock title='Prerequisites' elements={requirements.prereqs} />
+          ) : null}
+          {requirements.coreqs.length > 0 ? (
+            <InfoBlock title='Corequisites' elements={requirements.coreqs} />
+          ) : null}
+          {requirements.restrictions.length > 0 ? (
+            <InfoBlock
+              title='Restrictions'
+              elements={requirements.restrictions}
+            />
+          ) : null}
+          {requirements.otherInformation.length > 0 ? (
+            <InfoBlock
+              title='Other Information'
+              elements={requirements.otherInformation}
+            />
+          ) : null}
         </div>
       </div>
     </div>
