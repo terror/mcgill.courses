47e387e7ea20f20b475e9c42eaaadb4b94f1ee57
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-01 13:55:08 -0400

    format

--- client/src/components/CourseReview.tsx
+++ client/src/components/CourseReview.tsx
@@ -99,9 +99,9 @@ export const CourseReview = ({ review }: CourseReviewProps) => {
 // TODO: Align the rectangle to the right of the screen
 export const CourseRequirements = ({requirements}:  RequirementsProps) => {
   return (
-    <div className='w-30 mx-4 p-6 bg-slate-50 rounded-md'>
+    <div className='w-screen md:w-1/3 md:mt-10 mx-4 p-6 bg-slate-50 rounded-md'>
       <div className='flex-col space-y-3'>
-        <div className='space-y-2'>
+        <div className='space-y-7'>
         {requirements.prereqs.length > 0 && (
             <div>
               <h2 className='leading-none mt-1 font-semibold text-gray-700'>
