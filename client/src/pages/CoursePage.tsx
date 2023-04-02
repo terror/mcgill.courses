cad1858aeb4ce922e5ec56dfa6aeca01da6d45bf
Jeff Zhang <47371088+JeffDotPng@users.noreply.github.com>
2023-04-01 21:22:35 -0400

    Review form (#91)

--- client/src/pages/CoursePage.tsx
+++ client/src/pages/CoursePage.tsx
@@ -30,17 +30,15 @@ export const CoursePage = () => {
   }
 
   const review: Review = {
-    course: 'MATH 240',
+    courseId: 'MATH240',
     instructor: 'Adrian Roshan Vetta',
-    term: 'Fall 2022',
-    text:
+    content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
       'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' +
       'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
       'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    difficultyRating: 6,
-    usefulRating: 8,
-    interestingRating: 9,
+    userId: 'test',
+    rating: 4,
   };
 
   const requirements: Requirements = {
