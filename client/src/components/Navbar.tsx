5d4386754b9da9428e9b5735d094eaea71fadf91
Liam <liam@scalzulli.com>
2023-04-02 16:22:34 -0400

    Fix bird png path

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
       >
         <div className='flex lg:flex-1'>
           <a href='/' className='-m-1.5 p-1.5'>
-            <img className='h-12 w-auto' src='bird.png' alt='bird' />
+            <img className='h-12 w-auto' src='/bird.png' alt='bird' />
           </a>
         </div>
         <div className='flex lg:hidden'>
