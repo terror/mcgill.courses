e7575e664d0b9f0e2b3b0c08ffa2f0aef5e073af
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-02 12:20:18 -0400

    reformat course info

--- client/src/components/CourseInfo.tsx
+++ client/src/components/CourseInfo.tsx
@@ -25,26 +25,27 @@ export const CourseInfo = ({ course }: { course: Course }) => {
     <div className='w-screen p-6 bg-slate-50 md:mt-10'>
       <div className='flex flex-col md:flex-row'>
         <div className='flex flex-col space-y-3 w-fit m-4 md:w-1/2 md:m-4'>
-          <h1 className='text-xl font-semibold break-words text-gray-700'>
-            {course._id}: {course.title}
-          </h1>
+          <div className='flex flex-row space-x-2 align-middle'>
+            <h1 className='text-3xl font-semibold break-words text-gray-700'>
+              {course._id}
+            </h1>
+            {course.url ? <LinkButton url={course.url} /> : null}
+          </div>
+          <h2 className='text-2xl'> {course.title} </h2>
+
+          {/* Make nice UI for this*/}
+          {course.terms.length > 0 ? (
+            <p className='text-gray-500'>{course.terms.join(', ')}</p>
+          ) : (
+            <p> Not currently offered </p>
+          )}
           <p className='text-gray-500'>
-            {/* TODO: add links */}
             {course.faculty} ({course.department})
           </p>
+
           <p className='text-gray-500 break-words'>{course.description}</p>
-          <p className='text-gray-500'>Credits: {course.credits}</p>
-          {course.instructors.length > 0 ? (
-            <p className='text-gray-500'>
-              Instructors:{' '}
-              {course.instructors
-                .map((instructor) => `${instructor.name} (${instructor.term})`)
-                .join(', ')}
-            </p>
-          ) : null}
         </div>
-        <div className='flex flex-col space-y-3 w-fit m-4  md:m-4 md:w-1/2'>
-        </div>
+        <div className='flex flex-col space-y-3 w-fit m-4  md:m-4 md:w-1/2'></div>
       </div>
     </div>
   );
