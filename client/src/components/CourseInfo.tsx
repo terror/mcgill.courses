05df588c61012b587eb08078d43177d2c988aec7
JeffDotPng <zhangjeff2014@gmail.com>
2023-04-04 15:52:04 -0400

    add side margins on course info

--- client/src/components/CourseInfo.tsx
+++ client/src/components/CourseInfo.tsx
@@ -58,24 +58,26 @@ const CourseTerms = ({ course }: CourseTermsProps) => {
 
 export const CourseInfo = ({ course }: { course: Course }) => {
   return (
-    <div className='w-screen p-6 bg-slate-50 md:mt-10'>
-      <div className='flex flex-col md:flex-row'>
-        <div className='flex flex-col space-y-3 w-fit m-4 md:w-1/2 md:m-4'>
-          <div className='flex flex-row space-x-2 align-middle'>
-            <h1 className='text-4xl font-semibold break-words text-gray-800'>
-              {course._id}
-            </h1>
-            {course.url ? <LinkButton url={course.url} /> : null}
+    <div className='flex justify-center'>
+      <div className='w-screen p-6 mx-8 rounded-md bg-slate-50 md:mt-10'>
+        <div className='flex flex-col md:flex-row'>
+          <div className='flex flex-col space-y-3 w-fit m-4 md:w-1/2 md:m-4'>
+            <div className='flex flex-row space-x-2 align-middle'>
+              <h1 className='text-4xl font-semibold break-words text-gray-800'>
+                {course._id}
+              </h1>
+              {course.url ? <LinkButton url={course.url} /> : null}
+            </div>
+            <h2 className='text-3xl text-gray-800'> {course.title} </h2>
+            {course.terms.length > 0 ? (
+              <CourseTerms course={course} />
+            ) : (
+              <p>This course is not currently being offered.</p>
+            )}
+            <p className='text-gray-500 break-words'>{course.description}</p>
           </div>
-          <h2 className='text-3xl text-gray-800'> {course.title} </h2>
-          {course.terms.length > 0 ? (
-            <CourseTerms course={course} />
-          ) : (
-            <p>This course is not currently being offered.</p>
-          )}
-          <p className='text-gray-500 break-words'>{course.description}</p>
+          <div className='flex flex-col space-y-3 w-fit m-4  md:m-4 md:w-1/2'></div>
         </div>
-        <div className='flex flex-col space-y-3 w-fit m-4  md:m-4 md:w-1/2'></div>
       </div>
     </div>
   );
