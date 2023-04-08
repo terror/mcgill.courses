8d91d7c1836dff35394bfcf4f26c5d7d05cbe182
Liam <liam@scalzulli.com>
2023-04-08 19:22:50 -0400

    Add private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { PrivateRoute } from './components/PrivateRoute';
 import { AddReviewPage } from './pages/AddReviewPage';
 import { CoursePage } from './pages/CoursePage';
 import { EditReviewPage } from './pages/EditReviewPage';
@@ -15,8 +16,22 @@ const App = () => {
         <Route path=':id' element={<CoursePage />} />
       </Route>
       <Route path='review'>
-        <Route path=':id/add' element={<AddReviewPage />} />
-        <Route path=':id/edit' element={<EditReviewPage />} />
+        <Route
+          path=':id/add'
+          element={
+            <PrivateRoute>
+              <AddReviewPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path=':id/edit'
+          element={
+            <PrivateRoute>
+              <EditReviewPage />
+            </PrivateRoute>
+          }
+        />
       </Route>
       <Route path='/explore' element={<Explore />} />
       <Route path='/about' element={<About />} />
