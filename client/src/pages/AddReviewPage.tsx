7a68107465881db659948d7d3da2a97573142529
JeffDotPng <zhangjeff2014@gmail.com>
2023-04-09 17:08:30 -0400

    Redirect to edit when review already exists on client

--- client/src/pages/AddReviewPage.tsx
+++ client/src/pages/AddReviewPage.tsx
@@ -1,24 +1,31 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 
 import { AddReviewForm } from '../components/AddReviewForm';
 import { Layout } from '../components/Layout';
+import { useAuth } from '../hooks/useAuth';
 import { fetchClient } from '../lib/fetchClient';
 import { Course } from '../model/course';
+import { Review } from '../model/review';
 
 export const AddReviewPage = () => {
+  const user = useAuth();
   const params = useParams<{ id: string }>();
   const [course, setCourse] = useState<Course>();
+  const [review, setReview] = useState<Review>();
 
   useEffect(() => {
     fetchClient.getData<Course>(`courses/${params.id}`).then((data) => {
       setCourse(data);
+      fetchClient.getData<Review>(`reviews/${data._id}`).then((rev) => {
+        setReview(rev);
+      });
     });
   }, []);
 
-  if (!course) {
-    return <div></div>;
-  }
+  if (!course || review === undefined) return <div></div>;
+
+  if (review) return <Navigate to={`/review/${course._id}/edit`} />;
 
   return (
     <Layout>
