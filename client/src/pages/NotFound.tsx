3c77c007562bfad66d9e01daab6e6453c0101fc7
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-03 12:44:58 +0800

    remove unused imports

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,10 +1,7 @@
 import { Layout } from '../components/Layout';
-import { useNavigate } from 'react-router-dom';
 import { NotFound as NotFoundContent } from '../components/NotFound';
 
 export const NotFound = () => {
-  const nav = useNavigate();
-
   return (
     <Layout>
       <NotFoundContent />
