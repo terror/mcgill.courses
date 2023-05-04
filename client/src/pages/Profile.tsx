cfbb2be2782cb2efb452ce6ef41262edf47dfa01
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-04 16:16:29 +0800

    create profile page layout

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,9 +1,11 @@
 import { Layout } from '../components/Layout';
+import { useAuth } from '../hooks/useAuth';
 
 export const Profile = () => {
+  const user = useAuth();
   return (
     <Layout>
-      <h1>Profile</h1>
+      <h1>User: {user?.mail}</h1>
     </Layout>
   );
 };
