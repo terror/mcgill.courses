5f4c6b7fd7daf916efc4f1db246b88b8b5c8ce7f
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-06 08:49:22 +0800

    removed bad margin on layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout = ({ children }: LayoutProps) => {
 
   return (
     <div className={darkMode ? 'dark' : ''}>
-      <div className='mb-2 min-h-screen overflow-auto dark:bg-neutral-900'>
+      <div className='min-h-screen overflow-auto dark:bg-neutral-900'>
         <Navbar />
         <main>{children}</main>
       </div>
