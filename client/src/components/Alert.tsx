41196f47b8642fbe02eaf79c2ab7ea6e4bb6c08a
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-07 11:05:46 +0800

    cap alert z-index

--- client/src/components/Alert.tsx
+++ client/src/components/Alert.tsx
@@ -53,10 +53,10 @@ export const Alert = ({ status, message }: AlertProp) => {
     ${
       show
         ? 'translate-y-0 md:translate-y-0'
-        : 'translate-y-full md:translate-y-[150%]'
+        : 'md:translate-y-[150%] translate-y-full'
     }
     ${statusColor[status]}
-    fixed bottom-0 right-0 w-screen p-4 shadow-md transition-all duration-300 md:m-5 md:w-full md:max-w-md md:rounded-md
+    fixed bottom-0 right-0 z-50 w-screen p-4 shadow-md transition-all duration-300 md:m-5 md:w-full md:max-w-md md:rounded-md
   `}
       role='alert'
     >
