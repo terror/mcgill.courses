a5c9b32cf6dcd59455a57fe060a7f4983b076a9d
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-07 17:31:10 +0800

    method to sort block

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { Course } from '../model/Course';
 import { Instructor } from '../model/Instructor';
-import { Schedule } from '../model/Schedule';
+import { Block } from '../model/Schedule';
 
 export const classNames = (...classes: string[]) => {
   return classes.filter(Boolean).join(' ');
@@ -52,4 +52,26 @@ export const filterCurrentInstructors = (instructors: Instructor[]) => {
 
 export const dedupe = (arr: any[]) => {
   return [...new Set(arr)];
+};
+
+export const sortTerms = (terms: string[]) => {
+  const order = ['Fall', 'Winter', 'Summer'];
+  return terms.sort(
+    (a, b) => order.indexOf(a.split(' ')[0]) - order.indexOf(b.split(' ')[0])
+  );
+};
+
+export const sortBlocks = (blocks: Block[]) => {
+  const campusOrder = ['Downtown', 'Macdonald'];
+
+  return blocks.sort((a, b) => {
+    const aLec = parseInt(a.display.split(' ')[1], 10);
+    const bLec = parseInt(b.display.split(' ')[1], 10);
+
+    if (aLec === bLec) {
+      return campusOrder.indexOf(a.campus) - campusOrder.indexOf(b.campus);
+    }
+
+    return aLec - bLec;
+  });
 };
