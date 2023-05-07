0d5fb7621fd01fa88bb846312606b4b3c0dcabcc
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-07 16:19:01 +0800

    remove undefined fields in schedule model

--- client/src/model/Schedule.ts
+++ client/src/model/Schedule.ts
@@ -1,17 +1,17 @@
 export type Block = {
-  campus?: string;
-  display?: string;
-  location?: string;
+  campus: string;
+  display: string;
+  location: string;
   timeBlocks: TimeBlock[];
 };
 
 export type TimeBlock = {
-  day?: string;
-  t1?: string;
-  t2?: string;
+  day: string;
+  t1: string;
+  t2: string;
 };
 
 export type Schedule = {
-  blocks?: Block[];
-  term?: string;
+  blocks: Block[];
+  term: string;
 };
