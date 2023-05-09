86c48a288c7908ffc512ad07da0e155217144f77
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-10 06:15:33 +0800

    show all requirements even when none

--- client/src/components/CourseRequirements.tsx
+++ client/src/components/CourseRequirements.tsx
@@ -16,14 +16,20 @@ export const ReqsBlock = ({ title, reqs }: ReqsBlockProps) => {
       <h2 className='mb-2 mt-1 text-2xl font-medium leading-none text-gray-700 dark:text-gray-200'>
         {title}
       </h2>
-      {reqs.map((req) => (
-        <Link
-          to={`/course/${req.replace(' ', '')}`}
-          className='block text-gray-800 underline transition duration-100 hover:text-gray-600 dark:text-gray-200 hover:dark:text-gray-400'
-        >
-          {req}
-        </Link>
-      ))}
+      {reqs.length > 0 ? (
+        reqs.map((req) => (
+          <Link
+            to={`/course/${req.replace(' ', '')}`}
+            className='block text-gray-800 underline transition duration-100 hover:text-gray-600 dark:text-gray-200 hover:dark:text-gray-400'
+          >
+            {req}
+          </Link>
+        ))
+      ) : (
+        <p className='text-gray-500 dark:text-gray-400'>
+          This course has no {title.toLowerCase()}.
+        </p>
+      )}
     </div>
   );
 };
@@ -38,22 +44,18 @@ export const CourseRequirements = ({ requirements }: RequirementsProps) => {
     <div className='max-h-fit w-full rounded-md bg-slate-50 p-6 dark:bg-neutral-800 md:mx-4 md:ml-auto md:mr-8 md:mt-4'>
       <div className='flex-col space-y-3'>
         <div className='m-4 space-y-7'>
-          {requirements.prereqs.length > 0 ? (
-            <ReqsBlock title='Prerequisites' reqs={requirements.prereqs} />
-          ) : null}
-          {requirements.coreqs.length > 0 ? (
-            <ReqsBlock title='Corequisites' reqs={requirements.coreqs} />
-          ) : null}
-          {requirements.restrictions !== null ? (
-            <div>
-              <h2 className='mb-2 mt-1 text-2xl font-medium leading-none text-gray-700 dark:text-gray-200'>
-                Restrictions
-              </h2>
+          <ReqsBlock title='Prerequisites' reqs={requirements.prereqs} />
+          <ReqsBlock title='Corequisites' reqs={requirements.coreqs} />
+          <div>
+            <h2 className='mb-2 mt-1 text-2xl font-medium leading-none text-gray-700 dark:text-gray-200'>
+              Restrictions
+            </h2>
+            {requirements.restrictions !== null ? (
               <p className='text-gray-500 dark:text-gray-400'>
                 {requirements.restrictions}
               </p>
-            </div>
-          ) : null}
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
