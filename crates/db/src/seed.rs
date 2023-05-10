12444275c55aef398a8f6c04ff37e77436735f6b
Liam <liam@scalzulli.com>
2023-05-10 11:50:55 -0400

    Display source path when seeding the database

--- crates/db/src/seed.rs
+++ crates/db/src/seed.rs
@@ -2,43 +2,36 @@ use super::*;
 
 #[derive(Debug)]
 pub(crate) enum Seed {
-  Courses(Vec<Course>),
-  Reviews(Vec<Review>),
-  Unknown,
-}
-
-impl From<String> for Seed {
-  fn from(content: String) -> Self {
-    match (
-      serde_json::from_str::<Vec<Course>>(&content).ok(),
-      serde_json::from_str::<Vec<Review>>(&content).ok(),
-    ) {
-      (Some(courses), _) => Self::Courses(courses),
-      (_, Some(reviews)) => Self::Reviews(reviews),
-      _ => Self::Unknown,
-    }
-  }
+  Courses((PathBuf, Vec<Course>)),
+  Reviews((PathBuf, Vec<Review>)),
+  Unknown(PathBuf),
 }
 
 impl Seed {
   pub(crate) fn from_path(path: PathBuf) -> Result<Vec<Self>> {
-    if path.is_file() {
-      Ok(vec![Self::from(fs::read_to_string(path)?)])
+    Ok(if path.is_file() {
+      vec![Self::from_content(path.clone(), fs::read_to_string(path)?)]
     } else {
-      let mut paths = fs::read_dir(path)?
+      fs::read_dir(path)?
         .map(|path| -> Result<PathBuf> { Ok(path?.path()) })
-        .collect::<Result<Vec<_>>>()?;
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .sorted()
+        .map(|path| -> Result<Self> {
+          Ok(Self::from_content(path.clone(), fs::read_to_string(path)?))
+        })
+        .collect::<Result<Vec<_>>>()?
+    })
+  }
 
-      paths.sort();
-
-      Ok(
-        paths
-          .into_iter()
-          .map(|path| -> Result<Self> {
-            Ok(Self::from(fs::read_to_string(path)?))
-          })
-          .collect::<Result<Vec<_>>>()?,
-      )
+  fn from_content(path: PathBuf, content: String) -> Self {
+    match (
+      serde_json::from_str::<Vec<Course>>(&content).ok(),
+      serde_json::from_str::<Vec<Review>>(&content).ok(),
+    ) {
+      (Some(courses), _) => Self::Courses((path, courses)),
+      (_, Some(reviews)) => Self::Reviews((path, reviews)),
+      _ => Self::Unknown(path),
     }
   }
 }
