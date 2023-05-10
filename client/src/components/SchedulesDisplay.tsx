bc577f80d7030e47036cfb7f9c8db87ad7ac22c8
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-10 22:54:17 +0800

    return null in case of null schedules

--- client/src/components/SchedulesDisplay.tsx
+++ client/src/components/SchedulesDisplay.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import { TimeBlock, Block, Schedule } from '../model/Schedule';
-import { dedupe, sortTerms, sortBlocks, classNames } from '../lib/utils';
+import {
+  dedupe,
+  sortTerms,
+  sortBlocks,
+  classNames,
+  dedupeSchedulesBlocks,
+} from '../lib/utils';
 import { IoIosArrowDown } from 'react-icons/io';
 import { Transition } from '@headlessui/react';
 import { Course } from '../model/Course';
@@ -30,6 +36,7 @@ const VSBtimeToDisplay = (time: string) => {
   const approxTimeOfTheDay = parseInt(time, 10) / 60;
   const hour = Math.floor(approxTimeOfTheDay);
   const minute = Math.round((approxTimeOfTheDay - hour) * 60);
+
   return `${hour.toLocaleString('en-US', {
     minimumIntegerDigits: 2,
   })}:${minute.toLocaleString('en-US', {
@@ -40,6 +47,8 @@ const VSBtimeToDisplay = (time: string) => {
 
 export const SchedulesDisplay = ({ course }: { course: Course }) => {
   const schedules = course.schedule;
+
+  if (!schedules) return null;
 
   const offeredTerms = sortTerms(
     dedupe(schedules.map((schedule) => schedule.term))
@@ -73,6 +82,8 @@ export const SchedulesDisplay = ({ course }: { course: Course }) => {
     );
   };
 
+  let cache = new Set();
+
   return offeredTerms.length !== 0 ? (
     <div className='flex flex-col text-gray-800'>
       <div className='mx-8 mt-4 flex '>
@@ -95,79 +106,89 @@ export const SchedulesDisplay = ({ course }: { course: Course }) => {
       </div>
       <div className='mx-8 flex flex-col rounded-b-lg bg-neutral-100 dark:bg-neutral-700 dark:text-gray-200'>
         {currentlyDisplayingSchedules.map(
-          (schedule: Schedule, scheduleIndex) => (
-            <div key={scheduleIndex}>
-              {sortBlocks(schedule.blocks)?.map((block: Block, blockIndex) => (
-                <div key={blockIndex} className='flex flex-col'>
-                  <div
-                    className={classNames(
-                      'flex flex-row justify-between border-t border-neutral-200 p-2 px-3 pl-10 dark:border-neutral-600'
-                    )}
-                  >
-                    <div className='flex flex-wrap gap-x-3 whitespace-pre-wrap text-left'>
-                      <div className='w-20 '>
-                        <span className='font-semibold'>{block.display}</span>
-                      </div>
-                      <div className='w-44'>
-                        <span className='font-semibold'>Campus: </span>
-                        {block.campus}
-                      </div>
-                      <div className='w-56'>
-                        <span className='font-semibold'>Classroom(s): </span>
-
-                        {block.location
-                          ? block.location.replace(';', ',')
-                          : 'N/A'}
-                      </div>
-                    </div>
-                    <button
-                      onClick={() =>
-                        openBlock === block
-                          ? setOpenBlock(null)
-                          : setOpenBlock(block)
-                      }
-                    >
-                      <IoIosArrowDown
-                        className={`${
-                          openBlock === block ? 'rotate-180 transform' : ''
-                        } mx-2 h-5 w-5 text-gray-900 dark:text-gray-300`}
-                      />
-                    </button>
-                  </div>
-                  <Transition
-                    show={openBlock === block}
-                    enter='transition duration-300 ease-in-out transform'
-                    enterFrom='opacity-0 -translate-y-2'
-                    enterTo='opacity-100 translate-y-0'
-                    leave='transition duration-300 ease-in-out transform'
-                    leaveFrom='opacity-100 translate-y-0'
-                    leaveTo='opacity-0 -translate-y-2'
-                  >
-                    <div className='flex flex-col'>
-                      {block.timeblocks.length > 0 ? (
-                        block.timeblocks?.map((timeblock: TimeBlock, i) => (
-                          <div
-                            key={i}
-                            className='flex flex-row justify-between px-3 py-2 pl-10 font-medium text-gray-600 dark:text-neutral-300'
-                          >
-                            <p>{dayToWeekday(timeblock.day)}</p>
-                            <p>
-                              {VSBtimeToDisplay(timeblock.t1)} -{' '}
-                              {VSBtimeToDisplay(timeblock.t2)}
-                            </p>
+          (schedule: Schedule, scheduleIndex) => {
+            return (
+              <div key={scheduleIndex}>
+                {schedule.blocks?.map((block: Block, blockIndex) => {
+                  if (cache.has(block.display)) return null;
+                  cache.add(block.display);
+                  return (
+                    <div key={blockIndex} className='flex flex-col'>
+                      <div
+                        className={classNames(
+                          'flex flex-row justify-between border-t border-neutral-200 p-2 px-3 pl-10 dark:border-neutral-600'
+                        )}
+                      >
+                        <div className='flex flex-wrap gap-x-3 whitespace-pre-wrap text-left'>
+                          <div className='w-20 '>
+                            <span className='font-semibold'>
+                              {block.display}
+                            </span>
                           </div>
-                        ))
-                      ) : (
-                        <div className='flex flex-row justify-center px-3 py-2 dark:text-neutral-400'>
-                          <p>No scheduled time block.</p>
+                          <div className='w-44'>
+                            <span className='font-semibold'>Campus: </span>
+                            {block.campus}
+                          </div>
+                          <div className='w-56'>
+                            <span className='font-semibold'>
+                              Classroom(s):{' '}
+                            </span>
+
+                            {block.location
+                              ? block.location.replace(';', ',')
+                              : 'N/A'}
+                          </div>
                         </div>
-                      )}
+                        <button
+                          onClick={() =>
+                            openBlock === block
+                              ? setOpenBlock(null)
+                              : setOpenBlock(block)
+                          }
+                        >
+                          <IoIosArrowDown
+                            className={`${
+                              openBlock === block ? 'rotate-180 transform' : ''
+                            } mx-2 h-5 w-5 text-gray-900 dark:text-gray-300`}
+                          />
+                        </button>
+                      </div>
+                      <Transition
+                        show={openBlock === block}
+                        enter='transition duration-300 ease-in-out transform'
+                        enterFrom='opacity-0 -translate-y-2'
+                        enterTo='opacity-100 translate-y-0'
+                        leave='transition duration-300 ease-in-out transform'
+                        leaveFrom='opacity-100 translate-y-0'
+                        leaveTo='opacity-0 -translate-y-2'
+                      >
+                        <div className='flex flex-col'>
+                          {block.timeblocks.length > 0 ? (
+                            block.timeblocks?.map((timeblock: TimeBlock, i) => (
+                              <div
+                                key={i}
+                                className='flex flex-row justify-between px-3 py-2 pl-10 font-medium text-gray-600 dark:text-neutral-300'
+                              >
+                                <p>{dayToWeekday(timeblock.day)}</p>
+                                <p>
+                                  {VSBtimeToDisplay(timeblock.t1)} -{' '}
+                                  {VSBtimeToDisplay(timeblock.t2)}
+                                </p>
+                              </div>
+                            ))
+                          ) : (
+                            <div className='flex flex-row justify-center px-3 py-2 dark:text-neutral-400'>
+                              <p>No scheduled time block.</p>
+                            </div>
+                          )}
+                        </div>
+                      </Transition>
                     </div>
-                  </Transition>
-                </div>
-              ))}
-            </div>
-          )
+                  );
+                })}
+              </div>
+            );
+          }
         )}
       </div>
     </div>
