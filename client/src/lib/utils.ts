cc74b02815c9536f8f093550016a2a83e463cbb9
Sam Zhang <112342947+SamZhang02@users.noreply.github.com>
2023-05-13 11:24:06 +0800

    Sort and group course schedule information (#227)

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { Course } from '../model/Course';
 import { Instructor } from '../model/Instructor';
-import { Block } from '../model/Schedule';
+import { Block, Schedule } from '../model/Schedule';
 
 export const classNames = (...classes: string[]) => {
   return classes.filter(Boolean).join(' ');
@@ -54,24 +54,40 @@ export const dedupe = (arr: any[]) => {
   return [...new Set(arr)];
 };
 
-export const sortTerms = (terms: string[]) => {
-  const order = ['Fall', 'Winter', 'Summer'];
-  return terms.sort(
-    (a, b) => order.indexOf(a.split(' ')[0]) - order.indexOf(b.split(' ')[0])
-  );
+export const dedupeSchedulesByBlocks = (schedules: Schedule[]) => {
+  const deduped = [];
+  const filled = new Set();
+
+  for (const schedule of schedules) {
+    const block = schedule.blocks[0];
+    if (!filled.has(block.display)) {
+      deduped.push(schedule);
+      filled.add(block.display);
+    }
+  }
+
+  return deduped;
 };
 
-export const sortBlocks = (blocks: Block[]) => {
-  const campusOrder = ['Downtown', 'Macdonald'];
+export const sortTerms = (terms: string[]) => {
+  const order = ['Summer', 'Fall', 'Winter'];
+  return terms.sort((a, b) => {
+    return a.split(' ')[1] === b.split(' ')[1]
+      ? order.indexOf(a.split(' ')[0]) - order.indexOf(b.split(' ')[0])
+      : parseInt(a.split(' ')[1], 10) - parseInt(b.split(' ')[1], 10);
+  });
+};
 
-  return blocks.sort((a, b) => {
-    const aLec = parseInt(a.display.split(' ')[1], 10);
-    const bLec = parseInt(b.display.split(' ')[1], 10);
+export const sortSchedulesByBlocks = (schedules: Schedule[]) => {
+  const order = ['Lec', 'Lab', 'Seminar', 'Tut', 'Conf'];
+  return schedules.sort((a, b) => {
+    const aNum = parseInt(a.blocks[0].display.split(' ')[1], 10);
+    const bNum = parseInt(b.blocks[0].display.split(' ')[1], 10);
+    const aType = a.blocks[0].display.split(' ')[0];
+    const bType = b.blocks[0].display.split(' ')[0];
 
-    if (aLec === bLec) {
-      return campusOrder.indexOf(a.campus) - campusOrder.indexOf(b.campus);
-    }
-
-    return aLec - bLec;
+    return aType === bType
+      ? aNum - bNum
+      : order.indexOf(aType) - order.indexOf(bType);
   });
 };
