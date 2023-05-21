af442c0954e80266f3d926d3a6414e55e77323ab
Liam <liam@scalzulli.com>
2023-05-21 14:16:12 -0400

    Show 404 if the instructor doesn't exist

--- crates/db/src/db.rs
+++ crates/db/src/db.rs
@@ -403,7 +403,7 @@ impl Db {
   async fn add_instructor(&self, instructor: Instructor) -> Result {
     Ok(
       if self
-        .find_instructor(doc! { "name": &instructor.name })
+        .find_instructor_by_name(&instructor.name)
         .await?
         .is_none()
       {
@@ -433,6 +433,13 @@ impl Db {
         .find_one(query, None)
         .await?,
     )
+  }
+
+  pub async fn find_instructor_by_name(
+    &self,
+    name: &str,
+  ) -> Result<Option<Instructor>> {
+    self.find_instructor(doc! { "name": name }).await
   }
 
   #[cfg(test)]
