49672c18e4ddb307fae55f93a392aa23c1c3371b
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-06-04 14:33:08 +0800

    add average difficulty pie chart

--- client/src/components/RatingPieChart.tsx
+++ client/src/components/RatingPieChart.tsx
@@ -1,14 +1,9 @@
 import { PieChart } from 'react-minimal-pie-chart';
 
-type RatingPieChartProps = {
-  rating: number;
-  numReviews: number;
-};
-
-export const RatingPieChart = ({ rating, numReviews }: RatingPieChartProps) => {
+export const RatingPieChart = ({ rating }: { rating: number }) => {
   return (
     <div className='flex flex-col'>
-      <div className='relative z-10 mx-auto my-5 flex h-1/2 w-1/2 rounded-full '>
+      <div className='relative z-10 mx-auto my-5 flex h-1/2 w-3/5 rounded-full '>
         <PieChart
           data={[
             {
@@ -24,11 +19,6 @@ export const RatingPieChart = ({ rating, numReviews }: RatingPieChartProps) => {
         />
         <div className='absolute inset-0 z-20 mx-auto my-auto flex w-10/12 items-center justify-center text-xl font-semibold text-gray-700 dark:text-gray-300'>
           {Math.round(rating * 100) / 100} / 5
-        </div>
-      </div>
-      <div className='flex justify-between'>
-        <div className='mx-auto text-sm text-gray-500 dark:text-gray-400'>
-          {numReviews} review{numReviews === 1 ? '' : 's'}
         </div>
       </div>
     </div>
