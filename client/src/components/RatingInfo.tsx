40bb74835e0550ab92589ebd5ad92f6facf9a1ec
Sam Zhang <112342947+SamZhang02@users.noreply.github.com>
2023-06-10 13:19:31 +0800

    Add difficulty pie chart (#257)

--- client/src/components/RatingInfo.tsx
+++ client/src/components/RatingInfo.tsx
@@ -1,18 +1,23 @@
 import { RatingPieChart } from './RatingPieChart';
 
 export const RatingInfo = ({
+  title,
   rating,
   numReviews,
   content,
 }: {
+  title: string;
   rating: number;
   numReviews: number;
   content?: string;
 }) => {
   return (
     <div className='w-full lg:w-3/4'>
+      <h1 className='text-center text-xl font-semibold text-gray-800 dark:text-gray-200'>
+        {title}
+      </h1>
       {numReviews > 0 ? (
-        <RatingPieChart rating={rating} numReviews={numReviews} />
+        <RatingPieChart rating={rating} />
       ) : (
         <div className='text-left text-gray-700 dark:text-gray-200 md:text-center'>
           {content ??
