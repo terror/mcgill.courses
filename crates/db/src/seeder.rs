edf36e98aa647d83c228ae5637a94a22613701cb
liam <31192478+terror@users.noreply.github.com>
2023-06-11 23:56:46 -0400

    Simplify database seeding pipeline (#274)

--- crates/db/src/seeder.rs
+++ crates/db/src/seeder.rs
@@ -14,7 +14,7 @@ impl Seeder {
   pub(crate) async fn run(self) -> Result {
     info!("Seeding the database...");
 
-    for seed in Collector::new(&self.options.source).run()? {
+    for seed in self.collect()? {
       match seed {
         Seed::Courses((path, courses)) if !self.options.skip_courses => {
           info!("Seeding courses from {}...", path.display());
@@ -54,23 +54,48 @@ impl Seeder {
     Ok(())
   }
 
-  async fn seed<Item, Fut, F>(&self, items: Vec<Item>, runner: F) -> Result
-  where
-    Item: Clone + Send + 'static,
-    Fut: Future<Output = Result<(), anyhow::Error>> + Send + 'static,
-    F: Fn(Db, Item) -> Fut + Send + Sync + 'static,
-  {
-    if self.options.multithreaded {
-      let tasks = items.into_iter().map(|item| {
-        let db = self.db.clone();
-        tokio::task::spawn(runner(db, item))
-      });
+  fn collect(&self) -> Result<Vec<Seed>> {
+    Ok(if self.options.source.is_file() {
+      vec![Seed::from_content(
+        self.options.source.clone(),
+        fs::read_to_string(&self.options.source)?,
+      )]
+    } else {
+      fs::read_dir(&self.options.source)?
+        .map(|path| -> Result<PathBuf> { Ok(path?.path()) })
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .sorted()
+        .map(|path| -> Result<Seed> {
+          Ok(Seed::from_content(path.clone(), fs::read_to_string(path)?))
+        })
+        .collect::<Result<Vec<_>>>()?
+    })
+  }
 
-      for result in join_all(tasks).await {
-        if let Err(err) = result {
-          return Err(err.into());
-        }
-      }
+  async fn seed<
+    Item: Clone + Send,
+    Fut: Future<Output = Result> + Send + 'static,
+    F: Fn(Db, Item) -> Fut,
+  >(
+    &self,
+    items: Vec<Item>,
+    runner: F,
+  ) -> Result {
+    let tasks =
+      self
+        .options
+        .multithreaded
+        .then_some(items.clone().into_iter().map(|item| {
+          let db = self.db.clone();
+          tokio::task::spawn(runner(db, item))
+        }));
+
+    if let Some(tasks) = tasks {
+      join_all(tasks)
+        .await
+        .into_iter()
+        .collect::<Result<Vec<_>, _>>()?;
     } else {
       for item in items {
         runner(self.db.clone(), item).await?;
