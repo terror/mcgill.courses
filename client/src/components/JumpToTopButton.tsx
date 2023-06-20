41e9e8c2c035a3f12c64800786739a1cb7c474e3
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-19 23:55:00 -0400

    Fade in/out on JumpToTopButton

--- client/src/components/JumpToTopButton.tsx
+++ client/src/components/JumpToTopButton.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
+import { twMerge } from 'tailwind-merge';
 
 export const JumpToTopButton = () => {
   const [visible, setVisible] = useState(false);
@@ -16,12 +17,15 @@ export const JumpToTopButton = () => {
 
   window.addEventListener('scroll', toggleVisible);
 
-  return visible ? (
+  return (
     <button
-      className={`fixed bottom-10 right-10 z-50 rounded-full bg-gray-100 p-5 transition duration-150 ease-in-out hover:bg-gray-200 dark:bg-neutral-700 dark:text-gray-100 dark:hover:bg-neutral-600`}
+      className={twMerge(
+        !visible ? 'opacity-0' : 'opacity-100',
+        'fixed bottom-10 right-10 z-50 rounded-full bg-gray-100 p-5 transition duration-150 ease-in-out hover:bg-gray-200 dark:bg-neutral-700 dark:text-gray-100 dark:hover:bg-neutral-600'
+      )}
       onClick={scrollToTop}
     >
       <FaArrowUp size={20} />
     </button>
-  ) : null;
+  );
 };
