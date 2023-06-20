d87c404c02272ec4812b0fdf4117443280062765
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-19 23:20:42 -0400

    Cleanup ExploreFilter

--- client/src/components/ExploreFilter.tsx
+++ client/src/components/ExploreFilter.tsx
@@ -1,20 +1,12 @@
-import { Combobox, Transition } from '@headlessui/react';
 import { useState } from 'react';
 import { RefreshCw } from 'react-feather';
-import { GoX } from 'react-icons/go';
 import { twMerge } from 'tailwind-merge';
 
 import courseCodes from '../assets/courseCodes.json';
+import { MultiSelect } from './MultiSelect';
 
 const termsOptions = ['Fall', 'Winter', 'Summer'];
 const levelsOptions = ['1XX', '2XX', '3XX', '4XX', '5XX', '6XX', '7XX'];
-const codesOptions = courseCodes;
-
-type InputBoxProp = {
-  selected: string[];
-  setSelected: (selected: string[]) => void;
-  options: string[];
-};
 
 type ExploreFilterProp = {
   selectedSubjects: string[];
@@ -49,8 +41,10 @@ const FilterButton = ({
 
   return (
     <button
-      className={`mx-2 ml-0 rounded-full px-4 py-2 font-semibold tracking-wider transition duration-150 ease-in-out
-    ${selected ? selectedColor : unselectedColor}`}
+      className={twMerge(
+        'mx-2 ml-0 rounded-full px-4 py-2 font-semibold tracking-wider transition duration-150 ease-in-out',
+        selected ? selectedColor : unselectedColor
+      )}
       onClick={() => {
         setSelected(!selected);
         if (selected) {
@@ -65,134 +59,6 @@ const FilterButton = ({
   );
 };
 
-const CourseCodeBox = ({
-  selectedOption,
-  selectedOptions,
-  setSelectedOptions,
-}: {
-  selectedOption: string;
-  selectedOptions: string[];
-  setSelectedOptions: (selected: string[]) => void;
-}) => {
-  return (
-    <div className='mx-1 mt-4 flex h-8 w-28 items-center justify-end rounded-full bg-gray-100 dark:bg-neutral-700'>
-      <p className='mx-auto my-auto pl-3 text-xl font-medium tracking-wider text-black dark:text-gray-100'>
-        {selectedOption}
-      </p>
-      <button
-        className='my-auto ml-auto p-1 pr-2 text-black'
-        onClick={() =>
-          setSelectedOptions(
-            selectedOptions.filter(
-              (option) => option !== selectedOption && option !== ''
-            )
-          )
-        }
-      >
-        <GoX className='ml-0 h-5 w-5 duration-300 hover:text-red-600 dark:text-gray-100 dark:hover:text-red-700' />
-      </button>
-    </div>
-  );
-};
-
-const InputBox = ({ selected, setSelected, options }: InputBoxProp) => {
-  const [query, setQuery] = useState('');
-
-  const filteredData =
-    query === ''
-      ? options
-      : options.filter((data) => {
-          return data.toLowerCase().includes(query.toLowerCase());
-        });
-
-  return (
-    <div className='flex flex-col'>
-      <div className='rounded-full border dark:border-neutral-700'>
-        <Combobox
-          value={selected}
-          onChange={(val) => setSelected(val)}
-          multiple
-        >
-          {' '}
-          <div className='relative z-10 w-full cursor-default overflow-hidden rounded-full bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 dark:border-neutral-900 dark:bg-black sm:text-sm'>
-            <Combobox.Input
-              className='w-full border-none bg-neutral-50 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:outline-none dark:bg-neutral-800 dark:text-gray-200'
-              value={query}
-              onChange={(event) => setQuery(event.target.value)}
-              onKeyDown={(event) => {
-                if (event.key === 'Enter' && filteredData.length > 0) {
-                  setQuery('');
-                }
-              }}
-              autoComplete='off'
-            />
-          </div>
-          <Transition
-            enter='transition ease-in-out duration-100 transform'
-            enterFrom='opacity-0 scale-95'
-            enterTo='opacity-100 scale-100'
-            leave='transition ease-in-out duration-75 transform'
-            leaveFrom='opacity-100 scale-100'
-            leaveTo='opacity-0 scale-95'
-          >
-            <div className='absolute z-50 w-full bg-white'>
-              {query !== '' && (
-                <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-gray-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-neutral-800 sm:text-sm'>
-                  {filteredData.length > 0 ? (
-                    filteredData.map((data) => (
-                      <Combobox.Option key={data} value={data}>
-                        {({ active }) => (
-                          <div
-                            className={`${
-                              active
-                                ? 'bg-red-600 text-gray-100'
-                                : 'text-gray-700 dark:text-gray-100'
-                            } p-2 text-lg`}
-                            onClick={() => {
-                              setQuery('');
-                            }}
-                          >
-                            {data}
-                          </div>
-                        )}
-                      </Combobox.Option>
-                    ))
-                  ) : (
-                    <p className='p-2 text-lg text-gray-800 dark:text-gray-100'>
-                      Nothing Found.
-                    </p>
-                  )}
-                </Combobox.Options>
-              )}
-            </div>
-          </Transition>
-        </Combobox>
-      </div>
-    </div>
-  );
-};
-
-const SelectedCourseCodes = ({
-  selected,
-  setSelected,
-}: {
-  selected: string[];
-  setSelected: (selected: string[]) => void;
-}) => {
-  return selected.length > 0 ? (
-    <div className='flex flex-wrap'>
-      {selected.map((item, i) => (
-        <CourseCodeBox
-          key={i}
-          selectedOption={item}
-          selectedOptions={selected}
-          setSelectedOptions={(value) => setSelected(value)}
-        />
-      ))}
-    </div>
-  ) : null;
-};
-
 const ClearButton = ({
   setSelectedSubjects,
   setSelectedLevels,
@@ -203,7 +69,7 @@ const ClearButton = ({
   setSelectedTerms: (selected: string[]) => void;
 }) => {
   return (
-    <div className='ml-auto mr-10 mt-10 flex h-8 w-8 items-center justify-center rounded-full transition duration-200 hover:bg-gray-100 dark:hover:bg-neutral-700'>
+    <div className='ml-auto flex h-8 w-8 items-center justify-center rounded-full transition duration-200 hover:bg-gray-100 dark:hover:bg-neutral-700'>
       <button
         onClick={() => {
           setSelectedSubjects([]);
@@ -229,55 +95,52 @@ export const ExploreFilter = ({
   return (
     <div
       className={twMerge(
-        variant === 'mobile' ? 'mx-auto w-full' : 'ml-5 w-96 ',
-        'm-2 flex h-fit flex-col flex-wrap rounded-lg bg-slate-50 dark:bg-neutral-800 dark:text-gray-200'
+        variant === 'mobile' ? 'w-full' : 'w-96',
+        'flex h-fit flex-col flex-wrap rounded-lg bg-slate-50 px-10 py-8 dark:bg-neutral-800 dark:text-gray-200'
       )}
     >
       <div className='flex flex-row'>
-        <h1 className='m-10 mb-2 text-2xl font-semibold'>Filter</h1>
+        <h1 className='text-2xl font-semibold'>Filter</h1>
+        <div className='py-1' />
         <ClearButton
           setSelectedSubjects={setSelectedSubjects}
           setSelectedLevels={setSelectedLevels}
           setSelectedTerms={setSelectedTerms}
         />
       </div>
-      <div className='m-10 my-5'>
-        <div className='space-y-3'>
-          <h1 className='text-xl font-semibold'>Course Code</h1>
-          <InputBox
-            selected={selectedSubjects}
-            setSelected={(value) => setSelectedSubjects(value)}
-            options={codesOptions}
+      <div className='py-2.5' />
+      <h1 className='text-xl font-semibold'>Course Code</h1>
+      <div className='py-1.5' />
+      {/* TODO: Clicking on an option with a query in the field is kinda broken*/}
+      <MultiSelect
+        options={courseCodes}
+        values={selectedSubjects}
+        setValues={setSelectedSubjects}
+      />
+      <div>
+        <h1 className='mt-3 text-xl font-semibold'>Level</h1>
+        <div className='py-1' />
+        {levelsOptions.map((level, i) => (
+          <FilterButton
+            key={i}
+            name={level}
+            isSelected={selectedLevels.includes(level)}
+            selections={selectedLevels}
+            setSelections={setSelectedLevels}
           />
-        </div>
-        <SelectedCourseCodes
-          selected={selectedSubjects}
-          setSelected={setSelectedSubjects}
-        />
-        <div className='space-y-2'>
-          <h1 className='mt-3 text-xl font-semibold'>Level</h1>
-          {levelsOptions.map((level, i) => (
-            <FilterButton
-              key={i}
-              name={level}
-              isSelected={selectedLevels.includes(level)}
-              selections={selectedLevels}
-              setSelections={setSelectedLevels}
-            />
-          ))}
-        </div>
-        <div className='mb-8 space-y-2'>
-          <h1 className='mt-3 text-xl font-semibold'>Term</h1>
-          {termsOptions.map((term, i) => (
-            <FilterButton
-              key={i}
-              name={term}
-              isSelected={selectedTerms.includes(term)}
-              selections={selectedTerms}
-              setSelections={setSelectedTerms}
-            />
-          ))}
-        </div>
+        ))}
+      </div>
+      <div className='space-y-2'>
+        <h1 className='mt-3 text-xl font-semibold'>Term</h1>
+        {termsOptions.map((term, i) => (
+          <FilterButton
+            key={i}
+            name={term}
+            isSelected={selectedTerms.includes(term)}
+            selections={selectedTerms}
+            setSelections={setSelectedTerms}
+          />
+        ))}
       </div>
     </div>
   );
