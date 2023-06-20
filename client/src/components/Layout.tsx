dd376db4afe34035ae63ed1689f15c1bd6e87774
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-19 20:32:27 -0400

    Modify margins again

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -20,9 +20,7 @@ export const Layout = ({ children, preventScroll }: LayoutProps) => {
     <div className={darkMode ? 'dark' : ''}>
       <div className='transition-color min-h-screen overflow-auto pb-5 transition duration-300 ease-in-out dark:bg-neutral-900'>
         <Navbar />
-        <main className='lg:mx:40 mx-8 sm:mx-16 md:mx-24 xl:mx-48'>
-          {children}
-        </main>
+        <main className='mx-8 md:mx-16 lg:mx-24 xl:mx-40'>{children}</main>
       </div>
       <Footer />
     </div>
