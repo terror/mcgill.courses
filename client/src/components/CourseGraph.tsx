ff053bf7b3e5dad03b0265f8e98c844c3dd14f1f
Liam <liam@scalzulli.com>
2023-07-01 16:11:02 -0400

    Add leading courses to graph

--- client/src/components/CourseGraph.tsx
+++ client/src/components/CourseGraph.tsx
@@ -9,23 +9,36 @@ export const CourseGraph = ({ course }: { course: Course }) => {
 
   const [darkMode] = useDarkMode();
 
-  const prerequisites = course?.prerequisites?.map((prereq, i) => {
+  let id = 2;
+
+  const prerequisites = course.prerequisites.map((prereq) => {
     return {
-      id: i + 2,
+      id: id++,
       label: prereq.split(' ').join(''),
+    };
+  });
+
+  const leading = course.leadingTo.map((leading) => {
+    return {
+      id: id++,
+      label: leading,
     };
   });
 
   const graphNodes = [
     { id: 1, label: course?._id, title: course?.description },
-    ...(prerequisites || []),
+    ...prerequisites,
+    ...leading,
   ];
 
   const graph: GraphData = {
     nodes: graphNodes,
     edges: [
-      ...(prerequisites || []).map((c) => {
+      ...prerequisites.map((c) => {
         return { from: c.id, to: 1 };
+      }),
+      ...leading.map((l) => {
+        return { from: 1, to: l.id };
       }),
     ],
   };
@@ -43,7 +56,23 @@ export const CourseGraph = ({ course }: { course: Course }) => {
       options={{
         edges: { color: darkMode ? '#FFFFFF' : '#000000' },
         height: '500px',
-        layout: { hierarchical: false },
+        layout: {
+          randomSeed: undefined,
+          improvedLayout: true,
+          clusterThreshold: 150,
+          hierarchical: {
+            enabled: true,
+            levelSeparation: 150,
+            nodeSpacing: 100,
+            treeSpacing: 200,
+            blockShifting: true,
+            edgeMinimization: true,
+            parentCentralization: true,
+            direction: 'LR',
+            sortMethod: 'directed',
+            shakeTowards: 'leaves',
+          },
+        },
         nodes: { color: darkMode ? 'rgb(212 212 212)' : 'rgb(226 232 240)' },
       }}
       events={{
