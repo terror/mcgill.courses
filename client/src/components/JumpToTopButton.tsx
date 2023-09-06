2929c8210e4500d486393db3bf2b0e4ddced688e
JeffDotPng <zhangjeff2014@gmail.com>
2023-09-05 22:36:10 -0400

    fix color

--- client/src/components/JumpToTopButton.tsx
+++ client/src/components/JumpToTopButton.tsx
@@ -20,7 +20,7 @@ export const JumpToTopButton = () => {
     <button
       className={twMerge(
         !visible ? 'opacity-0' : 'opacity-100',
-        'fixed bottom-10 right-10 z-50 rounded-full bg-gray-100 p-5 transition duration-150 ease-in-out hover:bg-gray-200 dark:bg-neutral-700 dark:text-gray-100 dark:hover:bg-neutral-600'
+        'fixed bottom-10 right-10 z-50 rounded-full bg-gray-200 p-5 transition duration-150 ease-in-out hover:bg-gray-300 dark:bg-neutral-700 dark:text-gray-100 dark:hover:bg-neutral-600'
       )}
       disabled={!visible}
       onClick={scrollToTop}
