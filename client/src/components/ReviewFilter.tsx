c4f1e63fcf8e176952b9c058f580160f30d28f45
Liam <liam@scalzulli.com>
2023-09-15 01:33:20 -0400

    Add tweaks

--- client/src/components/ReviewFilter.tsx
+++ client/src/components/ReviewFilter.tsx
@@ -93,7 +93,7 @@ export const ReviewFilter = ({
         {({ open }) => (
           <>
             <Disclosure.Button>
-              <div className='flex w-full justify-between rounded-lg bg-gray-200 px-4 py-2 text-red-500 dark:bg-neutral-700'>
+              <div className='flex w-full justify-between rounded-lg bg-slate-200 px-4 py-2 text-red-500 dark:bg-neutral-700'>
                 <h1 className='text-sm font-medium text-gray-600 dark:text-gray-400'>
                   Filter...
                 </h1>
