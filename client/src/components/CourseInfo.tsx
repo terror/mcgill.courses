e1650c60bf792a0b8e6349a2960c46b27c491d5f
Liam <liam@scalzulli.com>
2023-09-22 16:25:05 -0400

    Tweak margins

--- client/src/components/CourseInfo.tsx
+++ client/src/components/CourseInfo.tsx
@@ -42,9 +42,9 @@ export const CourseInfo = ({ course, allReviews }: CourseInfoProps) => {
           {course.description}
         </p>
         <div className='grow py-3' />
-        <CourseInfoStats className='sm:hidden' allReviews={allReviews} />
+        <CourseInfoStats className='mb-4 sm:hidden' allReviews={allReviews} />
         <CourseInfoStats
-          className='hidden gap-x-6 sm:mb-4 sm:flex md:mb-0 md:hidden'
+          className='hidden gap-x-6 sm:mb-6 sm:flex md:mb-0 md:hidden'
           variant='medium'
           allReviews={allReviews}
         />
