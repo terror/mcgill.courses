769ac0d13f929466728445628b265740916ba36d
Liam <liam@scalzulli.com>
2023-09-24 12:58:16 -0400

    Add subscriptions tab to profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -16,6 +16,8 @@ import { Subscription } from '../model/Subscription';
 import { LuFileText } from 'react-icons/lu';
 import { VscBell } from 'react-icons/vsc';
 import { spliceCourseCode } from '../lib/utils';
+import { Tab } from '@headlessui/react';
+import { FaTrash } from 'react-icons/fa';
 
 export const Profile = () => {
   const user = useAuth();
@@ -46,6 +48,29 @@ export const Profile = () => {
         )
       );
   }, []);
+
+  const removeSubscription = async (courseId: string) => {
+    try {
+      await repo.removeSubscription(courseId);
+      setUserSubscriptions(
+        userSubscriptions?.filter(
+          (subscription) => subscription.courseId !== courseId
+        )
+      );
+      toast.success(
+        `Subscription for course ${spliceCourseCode(
+          courseId,
+          ' '
+        )} removed successfully.`
+      );
+    } catch (err) {
+      toast.error(
+        'An error occurred while removing your subscription, please try again later.'
+      );
+    }
+  };
+
+  const tabs = ['Reviews', 'Subscriptions'];
 
   return (
     <Layout>
@@ -86,45 +111,86 @@ export const Profile = () => {
           </div>
         </div>
       </div>
-      <div className='m-4'>
-        {userReviews === undefined ? (
-          <div className='mt-2 text-center'>
-            <Spinner />
-          </div>
-        ) : userReviews.length ? (
-          userReviews
-            .sort(
-              (a, b) =>
-                parseInt(a.timestamp.$date.$numberLong, 10) -
-                parseInt(b.timestamp.$date.$numberLong, 10)
-            )
-            .map((review, i) => {
-              return (
-                <div key={i}>
-                  <div className='flex'>
-                    <h2 className='flex-auto text-2xl font-bold text-gray-700 dark:text-gray-200'>
-                      {spliceCourseCode(review.courseId, ' ')}
-                    </h2>
-                    <Link
-                      to={`/course/${courseIdToUrlParam(review.courseId)}`}
-                      className='flex-auto text-right text-gray-700 underline hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50'
-                    >
-                      View Course
-                    </Link>
-                  </div>
-                  <div className='my-4 rounded-lg border-gray-800 duration-300 ease-in-out'>
-                    <CourseReview
-                      canModify={false}
-                      handleDelete={() => null}
-                      openEditReview={() => null}
-                      review={review}
-                    />
-                  </div>
+      <Tab.Group>
+        <Tab.List className='m-4 flex space-x-1 rounded-xl bg-neutral-700/20 p-1'>
+          {tabs.map((tab) => (
+            <Tab
+              key={tab}
+              className={({ selected }) =>
+                twMerge(
+                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-gray-700',
+                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-gray-400 focus:outline-none focus:ring-2',
+                  selected
+                    ? 'bg-white shadow'
+                    : 'text-gray-100 hover:bg-white/[0.12] hover:text-white'
+                )
+              }
+            >
+              {tab}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <div className='m-4'>
+              {userReviews === undefined ? (
+                <div className='mt-2 text-center'>
+                  <Spinner />
                 </div>
-              );
-            })
-        ) : null}
-      </div>
+              ) : userReviews.length ? (
+                userReviews
+                  .sort(
+                    (a, b) =>
+                      parseInt(a.timestamp.$date.$numberLong, 10) -
+                      parseInt(b.timestamp.$date.$numberLong, 10)
+                  )
+                  .map((review, i) => {
+                    return (
+                      <div key={i}>
+                        <div className='flex'>
+                          <h2 className='flex-auto text-2xl font-bold text-gray-700 dark:text-gray-200'>
+                            {spliceCourseCode(review.courseId, ' ')}
+                          </h2>
+                          <Link
+                            to={`/course/${courseIdToUrlParam(
+                              review.courseId
+                            )}`}
+                            className='flex-auto text-right text-gray-700 underline hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50'
+                          >
+                            View Course
+                          </Link>
+                        </div>
+                        <div className='my-4 rounded-lg border-gray-800 duration-300 ease-in-out'>
+                          <CourseReview
+                            canModify={false}
+                            handleDelete={() => null}
+                            openEditReview={() => null}
+                            review={review}
+                          />
+                        </div>
+                      </div>
+                    );
+                  })
+              ) : null}
+            </div>
+          </Tab.Panel>
+          <Tab.Panel>
+            <div>
+              {userSubscriptions?.map((subscription) => (
+                <div className='m-4 flex items-center rounded-lg border-gray-800 bg-white p-4 duration-300 ease-in-out dark:bg-neutral-800'>
+                  <p className='font-semibold text-gray-800 dark:text-gray-200'>
+                    {subscription.courseId}
+                  </p>
+                  <FaTrash
+                    onClick={() => removeSubscription(subscription.courseId)}
+                    className='ml-auto dark:text-white'
+                  />
+                </div>
+              ))}
+            </div>
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </Layout>
   );
 };
