80100373fd76725a97700dc9bd7c1f8fbd490f87
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-12-17 17:54:52 -0500

    update privacy policy

--- client/src/pages/PrivacyPolicy.tsx
+++ client/src/pages/PrivacyPolicy.tsx
@@ -60,24 +60,13 @@ export const PrivacyPolicy = () => {
           cannot guarantee its absolute security.
         </Paragraph>
 
-        <h2 className={h2}>Links to Other Sites</h2>
-
-        <Paragraph>
-          This Service may contain links to other sites. If you click on a
-          third-party link, you will be directed to that site. Note that these
-          external sites are not operated by us. Therefore, we strongly advise
-          you to review the Privacy Policy of these websites. We have no control
-          over and assume no responsibility for the content, privacy policies,
-          or practices of any third-party sites or services.
-        </Paragraph>
-
         <h2 className={h2}>Changes to This Privacy Policy</h2>
 
         <Paragraph>
           We may update our Privacy Policy from time to time. Thus, you are
           advised to review this page periodically for any changes. We will
           notify you of any changes by posting the new Privacy Policy on this
-          page. This policy is effective as of 2023-11-12.
+          page. This policy is effective as of 2023-12-17.
         </Paragraph>
 
         <h2 className={h2}>Contact Us</h2>
