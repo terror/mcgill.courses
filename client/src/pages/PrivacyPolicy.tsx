7ae15692d4879a9f48128974c2ec90567105eb3a
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-12-19 22:02:36 -0500

    shorten privacy policy

--- client/src/pages/PrivacyPolicy.tsx
+++ client/src/pages/PrivacyPolicy.tsx
@@ -14,11 +14,8 @@ export const PrivacyPolicy = () => {
           cost and is intended for use as is. This page is used to inform
           visitors regarding our policies with the collection, use, and
           disclosure of Personal Information if anyone decided to use our
-          Service. If you choose to use our Service, then you agree to the
-          collection and use of information in relation to this policy. We do
-          not store any personal information collected. We will not use or share
-          your information with anyone except as described in this Privacy
-          Policy. The terms used in this Privacy Policy have the same meanings
+          Service. If you choose to use our Service, then you agree to this
+          policy. The terms used in this Privacy Policy have the same meanings
           as in our Terms and Conditions, which are accessible at mcgill.courses
           unless otherwise defined in this Privacy Policy.
         </Paragraph>
@@ -39,18 +36,7 @@ export const PrivacyPolicy = () => {
           maintaining your session on our Service. We do not have access to or
           retain any personally identifiable information from Microsoft OAuth.
           You have the option to disable or delete these cookies through your
-          browser settings, but doing so may impact the functionality and
-          usability of our Service.
-        </Paragraph>
-
-        <h2 className={h2}>Security</h2>
-
-        <Paragraph>
-          We value your trust in providing us your Personal Information, thus we
-          are striving to use commercially acceptable means of protecting it.
-          But remember that no method of transmission over the internet, or
-          method of electronic storage is 100 % secure and reliable, and we
-          cannot guarantee its absolute security.
+          browser settings.
         </Paragraph>
 
         <h2 className={h2}>Changes to This Privacy Policy</h2>
