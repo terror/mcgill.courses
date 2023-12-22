d59df203e1ba5338163d25b4c04a6dcc032ce0b0
JeffDotPng <zhangjeff2014@gmail.com>
2023-12-22 17:18:01 -0400

    adjust whitespace

--- client/src/pages/TermsAndConditions.tsx
+++ client/src/pages/TermsAndConditions.tsx
@@ -2,7 +2,7 @@ import { Layout } from '../components/Layout';
 import { Paragraph } from '../components/Paragraph';
 
 export const TermsAndConditions = () => {
-  const h2 = 'text-2xl font-bold text-neutral-700 dark:text-gray-300';
+  const h2 = 'text-2xl font-bold text-neutral-700 dark:text-gray-300 mb-2';
   const h1 = 'text-4xl font-bold text-neutral-700 dark:text-gray-300';
 
   return (
@@ -14,53 +14,55 @@ export const TermsAndConditions = () => {
           By using mcgill.courses, these terms will automatically apply to you –
           you should make sure therefore that you read them carefully before
           using the website. As the website is open-sourced, You are allowed to
-          copy or modify the app as per the licence indicates.
+          copy or modify the app as the licence indicates.
         </Paragraph>
 
-        <h2 className={h2}>General Usage</h2>
+        <div>
+          <h2 className={h2}>General Usage</h2>
+          <Paragraph>
+            mcgill.courses is committed to ensuring that the website is as
+            useful and efficient as possible. For that reason, we reserve the
+            right to make changes to the website.
+          </Paragraph>
+        </div>
+
+        <div>
+          <h2 className={h2}>Community Guidelines</h2>
+          <Paragraph>
+            At mcgill.courses, we believe in fostering a positive and respectful
+            community environment. We encourage users to follow these community
+            guidelines when leaving reviews for courses offered at McGill
+            University:
+          </Paragraph>
+        </div>
 
         <Paragraph>
-          mcgill.courses is committed to ensuring that the website is as useful
-          and efficient as possible. For that reason, we reserve the right to
-          make changes to the website.
+          <b>Truthfulness:</b> All reviews must be truthful and accurately
+          reflect your own experiences and opinions. Fabricated or misleading
+          reviews are strictly prohibited.
         </Paragraph>
 
-        <h2 className={h2}>Community Guidelines</h2>
-
         <Paragraph>
-          At mcgill.courses, we believe in fostering a positive and respectful
-          community environment. We encourage users to follow these community
-          guidelines when leaving reviews for courses offered at McGill
-          University:
-        </Paragraph>
-
-        <Paragraph>
-          - Truthfulness: All reviews must be truthful and accurately reflect
-          your own experiences and opinions. Fabricated or misleading reviews
-          are strictly prohibited.
-        </Paragraph>
-
-        <Paragraph>
-          - Respect: Show respect towards instructors and fellow students. Avoid
-          personal attacks, hate speech, or any form of harassment in your
+          <b>Respect:</b> Show respect towards instructors and fellow students.
+          Avoid personal attacks, hate speech, or any form of harassment in your
           reviews or interactions.
         </Paragraph>
 
         <Paragraph>
-          - Constructive Feedback: Provide constructive feedback that can help
-          improve the course and the learning experience. Share your thoughts,
-          opinions, and suggestions in a respectful manner.
+          <b>Constructive Feedback:</b> Provide constructive feedback that can
+          help improve the course and the learning experience. Share your
+          thoughts, opinions, and suggestions in a respectful manner.
         </Paragraph>
 
         <Paragraph>
-          - Confidentiality: Do not disclose any confidential or private
+          <b>Confidentiality:</b> Do not disclose any confidential or private
           information about individuals or specific course content that may
           violate privacy or academic integrity.
         </Paragraph>
 
         <Paragraph>
-          - Non-Discrimination: Refrain from making discriminatory remarks or
-          engaging in any form of discrimination based on race, gender,
+          <b>Non-Discrimination:</b> Refrain from making discriminatory remarks
+          or engaging in any form of discrimination based on race, gender,
           religion, nationality, sexual orientation, or any other protected
           characteristic.
         </Paragraph>
@@ -74,14 +76,15 @@ export const TermsAndConditions = () => {
           understand the consequences for violating them.
         </Paragraph>
 
-        <h2 className={h2}>Changes to This Terms and Conditions</h2>
-
-        <Paragraph>
-          We may update our Terms and Conditions from time to time. Thus, you
-          are advised to review this page periodically for any changes. If you
-          have any questions or suggestions about our Terms and Conditions, do
-          not hesitate to contact us at admin@mcgill.courses.
-        </Paragraph>
+        <div>
+          <h2 className={h2}>Changes to these Terms and Conditions</h2>
+          <Paragraph>
+            We may update our Terms and Conditions from time to time. Thus, you
+            are advised to review this page periodically for any changes. If you
+            have any questions or suggestions about our Terms and Conditions, do
+            not hesitate to contact us at admin@mcgill.courses.
+          </Paragraph>
+        </div>
       </div>
     </Layout>
   );
