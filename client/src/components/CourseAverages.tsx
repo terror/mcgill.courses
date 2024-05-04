c40cd8a8bfadb66f1df6a178811245878c564083
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-05-03 20:14:23 -0400

    UI adjustments on course averages

--- client/src/components/CourseAverages.tsx
+++ client/src/components/CourseAverages.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+
 import { compareTerms } from '../lib/utils';
 import { TermAverage } from '../model/TermAverage';
 
@@ -6,34 +9,46 @@ type CourseAveragesProps = {
 };
 
 export const CourseAverages = ({ averages }: CourseAveragesProps) => {
+  const [showAll, setShowAll] = useState<boolean>(false);
+
   return (
     <div
       className={
         'relative w-full rounded-md bg-slate-50 p-5 shadow-sm dark:bg-neutral-800'
       }
     >
-      <h2 className='mb-2 mt-1 text-xl font-bold leading-none text-gray-700 dark:text-gray-200'>
+      <h2 className='mb-2 mt-1 text-lg font-bold leading-none text-gray-700 dark:text-gray-200 md:text-xl'>
         Class Averages
       </h2>
 
       <hr className='my-2 mt-5 w-full border border-neutral-200 dark:border-neutral-700' />
 
       {averages
-        .sort((a, b) => compareTerms(a.term, b.term))
-        .reverse()
+        .sort((a, b) => compareTerms(b.term, a.term))
+        .slice(0, showAll ? averages.length : 6)
         .map((average) => (
           <>
-            <div className='flex flex-row '>
-              <p className='w-11/12 text-lg text-gray-500 dark:text-gray-400'>
-                {average.term}
-              </p>
-              <p className='text-lg text-gray-500 dark:text-gray-400'>
-                {average.average}
-              </p>
+            <div className='flex flex-row text-sm font-medium text-gray-500 dark:text-gray-400 md:text-lg'>
+              <p className='w-11/12'>{average.term}</p>
+              <p>{average.average}</p>
             </div>
             <hr className='my-2 w-full border border-neutral-200 dark:border-neutral-700' />
           </>
         ))}
+
+      <button
+        className='flex w-full items-center gap-2 text-sm text-gray-500 dark:text-gray-400 md:text-lg'
+        onClick={() => setShowAll(!showAll)}
+      >
+        <p className='my-auto ml-auto text-base font-medium'>
+          {showAll ? 'Show less' : 'Show all'}
+        </p>
+        {showAll ? (
+          <IoIosArrowUp className='my-auto mr-auto font-extrabold' />
+        ) : (
+          <IoIosArrowDown className='my-auto mr-auto font-extrabold' />
+        )}
+      </button>
 
       <p className='mt-5 text-center text-sm text-gray-700 dark:text-gray-200'>
         Supported by{' '}
