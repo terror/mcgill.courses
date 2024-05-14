be6031586619fbad0b90c61f0022cc443ed9fddf
Liam <liam@scalzulli.com>
2024-05-14 15:17:00 -0400

    Place after about page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,8 +4,8 @@ export const navigationItems = [
   { name: 'Home', href: '/' },
   { name: 'Explore', href: '/explore' },
   { name: 'Reviews', href: '/reviews' },
-  { name: 'Changelog', href: '/changelog' },
   { name: 'About', href: '/about' },
+  { name: 'Changelog', href: '/changelog' },
 ];
 
 export const Footer = () => {
