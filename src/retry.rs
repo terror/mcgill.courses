d1c45cdec08f3417655a926885e838de3402aabe
Liam <liam@scalzulli.com>
2025-02-01 17:42:12 -0500

    Use exponential backoff with jitter when retrying requests

--- src/retry.rs
+++ src/retry.rs
@@ -1,14 +1,17 @@
 use super::*;
 
 pub(crate) trait Retry {
-  fn retry(self, retries: usize) -> Result<reqwest::blocking::Response>;
+  fn retry(self, max_retries: usize) -> Result<reqwest::blocking::Response>;
 }
 
 impl Retry for RequestBuilder {
-  fn retry(self, retries: usize) -> Result<reqwest::blocking::Response> {
+  fn retry(self, max_retries: usize) -> Result<reqwest::blocking::Response> {
     let mut attempts = 0;
 
-    while attempts <= retries {
+    let (base_delay, max_delay) =
+      (Duration::from_millis(100), Duration::from_secs(30));
+
+    while attempts <= max_retries {
       match self
         .try_clone()
         .ok_or_else(|| anyhow!("Failed to clone request builder"))?
@@ -16,13 +19,36 @@ impl Retry for RequestBuilder {
       {
         Ok(response) => return Ok(response),
         Err(error) => {
-          error!("Request failed: {error}");
+          error!(
+            "Request failed (attempt {}/{}): {}",
+            attempts + 1,
+            max_retries + 1,
+            error
+          );
+
+          if attempts == max_retries {
+            break;
+          }
+
+          let (exp_delay, jitter) = (
+            base_delay * (2_u32.pow(attempts as u32)),
+            Duration::from_millis(rand::random::<u64>() % 100),
+          );
+
+          let delay = std::cmp::min(exp_delay + jitter, max_delay);
+
+          warn!("Retrying in {:?}...", delay);
+
+          thread::sleep(delay);
+
           attempts += 1;
-          thread::sleep(Duration::from_secs(1));
         }
       }
     }
 
-    Err(Error(anyhow!("Request timed out")))
+    Err(Error(anyhow!(
+      "Request failed after {} attempts",
+      max_retries + 1
+    )))
   }
 }
