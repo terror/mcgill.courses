921f6c3dcdf12045fa6154d5dbb5a385b36e32fd
39bytes <jeff@39bytes.dev>
2025-05-09 17:18:22 -0700

    remove course_listing.rs

--- crates/model/src/course_listing.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
-pub struct CourseListing {
-  pub department: Option<String>,
-  pub faculty: Option<String>,
-  pub level: Option<String>,
-  pub terms: Vec<String>,
-  pub url: String,
-}
