1466e8953b91f983ebb3d01f3f1ee8a7be5eb109
Liam <liam@scalzulli.com>
2025-06-19 13:25:07 -0400

    Show location modal on click for course schedule buildings

--- client/src/components/SchedulesDisplay.tsx
+++ client/src/components/SchedulesDisplay.tsx
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { useEffect, useMemo, useState } from 'react';
+import { Fragment, useEffect, useMemo, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { toast } from 'sonner';
 import { twMerge } from 'tailwind-merge';
@@ -8,6 +8,7 @@ import * as buildingCodes from '../assets/buildingCodes.json';
 import { getCurrentTerm, sortTerms } from '../lib/utils';
 import type { Course } from '../model/Course';
 import type { Block, Schedule } from '../model/Schedule';
+import { BuildingLocation } from './BuildingLocation';
 import { Tooltip } from './Tooltip';
 
 const VSBtimeToDisplay = (time: string) => {
@@ -71,16 +72,29 @@ const getSections = (
 };
 
 const BlockLocation = ({ location }: { location: string }) => {
-  const room = location.split(' ')[0];
+  const code = location.split(' ')[0];
+
+  const [isLocationOpen, setIsLocationOpen] = useState(false);
 
   return (
-    <span className='relative whitespace-nowrap'>
-      <Tooltip text={buildingCodes[room as keyof typeof buildingCodes]}>
-        <p className='inline-block cursor-default text-sm leading-7 sm:text-base'>
-          {location}
-        </p>
-      </Tooltip>
-    </span>
+    <Fragment>
+      <span
+        className='relative whitespace-nowrap'
+        onClick={() => setIsLocationOpen(true)}
+      >
+        <Tooltip text={buildingCodes[code as keyof typeof buildingCodes]}>
+          <p className='inline-block cursor-pointer text-sm leading-7 sm:text-base'>
+            {location}
+          </p>
+        </Tooltip>
+      </span>
+      <BuildingLocation
+        title={buildingCodes[code as keyof typeof buildingCodes]}
+        code={code}
+        open={isLocationOpen}
+        onClose={() => setIsLocationOpen(false)}
+      />
+    </Fragment>
   );
 };
 
--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,3 +1,6 @@
-const { VITE_API_URL } = import.meta.env;
+const { VITE_API_URL, VITE_GOOGLE_API_KEY } = import.meta.env;
 
-export const env = { VITE_API_URL: VITE_API_URL ?? '' };
+export const env = {
+  VITE_API_URL: VITE_API_URL ?? '',
+  VITE_GOOGLE_API_KEY: VITE_GOOGLE_API_KEY ?? '',
+};
