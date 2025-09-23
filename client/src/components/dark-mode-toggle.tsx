19c07cf670ec4c27d5c7e4e786d681af5caa5822
Liam <liam@scalzulli.com>
2025-09-23 16:25:24 -0400

    Tweak

--- client/src/components/dark-mode-toggle.tsx
+++ client/src/components/dark-mode-toggle.tsx
@@ -12,7 +12,7 @@ export const DarkModeToggle = () => {
   return (
     <button
       onClick={toggleDarkMode}
-      className='rounded-2xl p-1 hover:bg-gray-50 dark:hover:bg-neutral-700'
+      className='rounded-2xl p-1.5 hover:bg-gray-50 dark:hover:bg-neutral-700'
     >
       {darkMode ? (
         <Sun className='size-6 fill-gray-400 stroke-gray-400 stroke-2' />
