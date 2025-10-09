d9ebf70c06f7242c1907cfa9a0c06c61eb262d73
liam <liam@scalzulli.com>
2025-10-09 18:09:08 -0400

    Add documentation for user related endpoints (#724)

--- src/documentation.rs
+++ src/documentation.rs
@@ -54,7 +54,8 @@ impl Modify for MicrosoftOAuthSecurity {
     subscriptions::get_subscription,
     subscriptions::add_subscription,
     subscriptions::delete_subscription,
-    search::search
+    search::search,
+    user::get_user
   ),
   components(
     schemas(
@@ -73,6 +74,8 @@ impl Modify for MicrosoftOAuthSecurity {
       subscriptions::AddOrDeleteSubscriptionBody,
       subscriptions::GetSubscriptionParams,
       subscriptions::SubscriptionResponse,
+      user::User,
+      user::UserResponse,
     )
   ),
   tags(
@@ -82,6 +85,7 @@ impl Modify for MicrosoftOAuthSecurity {
     (name = "reviews", description = "All review related endpoints."),
     (name = "search", description = "All search related endpoints."),
     (name = "subscriptions", description = "All subscription related endpoints."),
+    (name = "user", description = "Endpoints related to user authentication state."),
   ),
 )]
 pub(crate) struct Documentation;
